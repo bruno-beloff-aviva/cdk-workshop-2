abcac50ba1c2ea2773e95b2edd98b08ba935db9c
bruno-beloff-aviva <bruno.beloff@aviva.com>
2025-02-04 16:22:30 +0000

    Removed cdk-standards

--- cdk-workshop.go
+++ cdk-workshop.go
@@ -8,7 +8,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/aviva-verde/cdk-standards/s3"
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
@@ -25,6 +24,28 @@ const project = "CDK2"
 const bucketName = "cdk2-hello-bucket"
 const objectName = "hello.txt"
 
+type BucketLogConfiguration struct {
+	BucketName string
+	Region     string
+	LogPrefix  string
+}
+
+type BucketProps struct {
+	Stack awscdk.Stack
+	Name  string
+	// OverrideFunctionName [optional] override the bucket name used for the bucket instead of setting the name and ID the same
+	OverrideBucketName *string
+	Versioned          bool
+	LogConfiguration   BucketLogConfiguration
+	LifecycleRules     []*awss3.LifecycleRule
+	EventBridgeEnabled bool
+}
+
+type PublicBucketProps struct {
+	BucketProps
+	Cors []*awss3.CorsRule
+}
+
 type CdkWorkshopStackProps struct {
 	awscdk.StackProps
 }
@@ -40,12 +61,12 @@ func NewCdkTable(scope constructs.Construct, id string) awsdynamodb.Table {
 }
 
 func NewHelloBucket(stack awscdk.Stack, name string) awss3.Bucket { // using cdk-standards
-	logConfig := s3.BucketLogConfiguration{
+	logConfig := BucketLogConfiguration{
 		BucketName: name,
 		LogPrefix:  "HelloLogPrefix",
 	}
 
-	props := s3.BucketProps{
+	props := BucketProps{
 		Stack:              stack,
 		Name:               name + "-props",
 		OverrideBucketName: aws.String(name),
@@ -56,7 +77,7 @@ func NewHelloBucket(stack awscdk.Stack, name string) awss3.Bucket { // using cdk
 
 	fmt.Printf("props: %#v\n", props)
 
-	return s3.NewPrivateS3Bucket(props)
+	return NewPrivateS3Bucket(props)
 }
 
 func NewHelloHandler(stack awscdk.Stack, lambdaEnv map[string]*string) awslambdago.GoFunction {
@@ -124,4 +145,25 @@ func main() {
 	NewCdkWorkshopStack(app, project+"WorkshopStack", &CdkWorkshopStackProps{})
 
 	app.Synth(nil)
+}
+
+func newS3Bucket(props BucketProps) awss3.Bucket {
+	return awss3.NewBucket(props.Stack, jsii.String(props.Name), &awss3.BucketProps{
+		Encryption:        awss3.BucketEncryption_S3_MANAGED,
+		LifecycleRules:    &props.LifecycleRules,
+		BlockPublicAccess: awss3.BlockPublicAccess_BLOCK_ALL(),
+		Versioned:         jsii.Bool(props.Versioned),
+		EnforceSSL:        jsii.Bool(true),
+		ServerAccessLogsBucket: awss3.Bucket_FromBucketAttributes(props.Stack, jsii.String(fmt.Sprintf("%s-s3AccessLogs", props.Name)), &awss3.BucketAttributes{
+			BucketName: jsii.String(props.LogConfiguration.BucketName),
+			Region:     jsii.String(props.LogConfiguration.Region),
+		}),
+		ServerAccessLogsPrefix: jsii.String(props.LogConfiguration.LogPrefix),
+		EventBridgeEnabled:     jsii.Bool(props.EventBridgeEnabled),
+		BucketName:             props.OverrideBucketName,
+	})
+}
+
+func NewPrivateS3Bucket(props BucketProps) awss3.Bucket {
+	return newS3Bucket(props)
 }
